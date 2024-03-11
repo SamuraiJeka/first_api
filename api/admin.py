f16f985ba2bb363724207f31b4136297a3868802
SamuraiJeka <offissit123321@gmail.com>
2024-03-11 23:43:02 +0300

    Модели и админка

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model=User
+    list_display=[
+        "username",
+        "email",
+        "is_staff",
+    ]
+    list_filter=[
+        "is_staff"
+    ]
+    fields=[
+        "username",
+        "password",
+        "email",
+    ]
+    readonly_fields=[
+        "username",
+        "password",
+        "email",
+    ]
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    model=Item
+    list_display=[
+        "name",
+        "price",
+        "imageURL"
+    ]
+    fields=[
+        "name",
+        "description",
+        "price",
+        "imageURL",
+    ]
+    readonly_fields=["get_image"]
+
+    def get_image(self, item: Item):
+        if item.imageURL:
+            return mark_safe(f"<img src='{item.imageURL.url}' width=50 length=60>")
+        else:
+            return "Без изображения"
+        
+    get_image.short_description="Изображение"
+
+@admin.register(Cart)
+class CatrAdmin(admin.ModelAdmin):
+    model=Cart
+    list_display=[
+        "user",
+        "item",
+        "count",
+    ]
--- api/models.py
+++ api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    def __str__(self):
+        return self.username
+
+    class Meta(AbstractUser.Meta):
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        swappable = "AUTH_USER_MODEL"
+
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=32,
+        unique=True,
+        verbose_name="Название"
+    )
+    imageURL=models.ImageField(
+        upload_to="static/",
+        verbose_name="Изображене"
+    )
+    description=models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=True
+    )
+    price=models.PositiveIntegerField(
+        verbose_name="Цена",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+class Cart(models.Model):
+    user=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        unique=False,
+        verbose_name="Пользователь",
+    )
+    item=models.ForeignKey(
+        Item,
+        on_delete=models.CASCADE,
+        unique=False,
+        verbose_name="Товар",
+    )
+    count=models.SmallIntegerField(
+        verbose_name="Количество"
+    )
+
+    class Meta:
+        verbose_name="Корзина"
+        verbose_name_plural="Корзины"
