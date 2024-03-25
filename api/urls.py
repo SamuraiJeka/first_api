2a371597edd6132c8654bf81a64347e2ff9ad009
SamuraiJeka <offissit123321@gmail.com>
2024-03-25 10:54:53 +0300

    user api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from api.views import ItemAPI
+from api.views import ItemViewset, UserViewset
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r"api/items", ItemAPI)
+router.register(r"api/items", ItemViewset)
+router.register(r"api/users", UserViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics, mixins
 
-from .models import Item
-from .seriailzers import ItemSerializer
+from .models import Item, User
+from .seriailzers import ItemSerializer, UserSerializer
 
 
-class ItemAPI(viewsets.ModelViewSet):
+class ItemViewset(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin):
+    
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    http_method_names = ['get']
+
+class UserViewset(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
